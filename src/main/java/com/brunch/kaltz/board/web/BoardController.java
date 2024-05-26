9dd24d9146bf548934ba4c8b6fbc053374794afb
kaltz <rmsdhcjswo1@gmail.com>
2024-05-26 12:51:52 +0900

    http header manipulation

--- src/main/java/com/brunch/kaltz/board/web/BoardController.java
+++ src/main/java/com/brunch/kaltz/board/web/BoardController.java
@@ -3,12 +3,21 @@ package com.brunch.kaltz.board.web;
 import com.brunch.kaltz.board.domain.Board;
 import com.brunch.kaltz.board.domain.BoardDto;
 import com.brunch.kaltz.board.service.BoardService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 
+@Slf4j
 @Controller
 public class BoardController {
 
@@ -19,8 +28,45 @@ public class BoardController {
     }
 
     @GetMapping("/board")
-    public String openBoard(Model model) {
+    public String openBoard(Model model, HttpServletResponse response) {
         // Get Method : 리소스를 사용자에게 전달하라
+
+        // header 조작
+        /**
+         * 내가 설정해주는 헤더의 이름이 기존 헤더의 이름과 같다면
+         * 내가 설정해준 값을 대체 된다.
+         */
+        response.addHeader("addBasic", "world"); // 응답 헤더에 Hello : world 를 추가
+        response.addDateHeader("addDate", 20L);
+        Cookie cookie = new Cookie("myCookie", "myCookieValue");
+        response.addCookie(cookie);
+        response.addIntHeader("addInt", 10);
+        boolean isContainsHeader = response.containsHeader("Host");
+        log.info("isContainsHeader = {}", isContainsHeader);
+        String encodeRedirectURL = response.encodeRedirectURL("https://speakerdeck.com/weirdx/99con-junieo-gaebaljayi-iryeogseo-sseugi-idongug?slide=51");
+        log.info("encodeRedirectURL = {}", encodeRedirectURL);
+        String encodeURL = response.encodeURL("https://speakerdeck.com/weirdx/99con-junieo-gaebaljayi-iryeogseo-sseugi-idongug?slide=51");
+        log.info("encodeURL = {}", encodeURL);
+
+        /*
+        try {
+            // 리다이렉트 코드
+            response.sendRedirect("https://speakerdeck.com/weirdx/99con-junieo-gaebaljayi-iryeogseo-sseugi-idongug?slide=51");
+        } catch (IOException e) {
+            String message = e.getMessage();
+            log.error("message={}", message);
+        }
+        */
+        response.setStatus(301);
+
+        /**
+         * Lazy Evaluation : "게으른 연산"으로, 불필요한 연산을 피하기 위해
+         * 연산을 지연시키는 것
+         * 즉 불필요한 연산을 피한다
+          */
+        Supplier<Map<String, String>> mapSupplier =
+                () -> new HashMap<>();
+        response.setTrailerFields(mapSupplier);
 
         // 게시판의 모든 데이터를 조회해 오기
         List<Board> resultList = boardService.findAll();
--- src/main/java/com/brunch/kaltz/junittest/UserService.java
+++ src/main/java/com/brunch/kaltz/junittest/UserService.java
@@ -1,8 +1,11 @@
 package com.brunch.kaltz.junittest;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserService {
     public UserResponse signUp(SignUpRequest request) {
         return new UserResponse();
